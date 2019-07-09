d8428f89b2dc3cc2df48d9968bd2370c9f5e7055
ketakrs <ketakrs@gmail.com>
2019-07-09 14:04:18 +0530

    update dburl

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ var commentRoutes 	 = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
 	indexRoutes 	 = require("./routes/index");
 
-//
-//mongoose.connect("mongodb://localhost/yelp_camp_v12");
-mongoose.connect('mongodb+srv://rusty:passwd@yelpcamp-fvqvg.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+//mongoose.connect('mongodb+srv://rusty:passwd@yelpcamp-fvqvg.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname +"/public"));
